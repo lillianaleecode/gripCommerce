0dae004a4178e16941b72c13cc03c90c4c5bfe78
lillianaleecode <lilliana.lee@code.berlin>
2022-06-02 16:07:09 +0200

    NavBar. structuring basic navbar by styling it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,43 @@ import React from 'react'
 import styled from 'styled-components'
 
 //Styles: (using styled-components)
-const Container = styled.div`
+const Container = styled.div
+`
     height:60px;
     background-color: magenta;
 `
+const Wrapper = styled.div
+`
+    display: flex;
+    justify-content: space-between;
 
+`
+const Left = styled.div`
+    flex: 1;
+`
+
+const Center = styled.div`
+    flex: 1;
+`
+const Right = styled.div`
+    flex: 1;
+`
 
 const Navbar = () => {
   return (
     //<div>Navbar!</div> //instead of doing it like this, use styled-components to style css it.
-    <Container>Navbar!</Container>
+    <Container>
+        <Wrapper>
+            <Left>
+                <p>left</p>
+            </Left>
+            <Center>Center</Center>
+            <Right>
+                <p>right</p>
+            </Right>
+        </Wrapper>
+
+    </Container>
 
   )
 }
