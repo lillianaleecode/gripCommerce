a0caba7e109e73a239b9f67a1f32004464aa9322
lillianaleecode <lilliana.lee@code.berlin>
2022-06-15 15:23:18 +0200

    Email Newsletter Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Announcement from '../components/Announcement'
 import Categories from '../components/Categories'
 import Navbar from '../components/Navbar'
+import Newsletter from '../components/Newsletter'
 import Slider from '../components/Slider'
 
 export const Home = () => {
@@ -11,6 +12,7 @@ export const Home = () => {
       <Navbar/>
       <Slider/>
       <Categories/>
+      <Newsletter/>
 
     </div>
     
