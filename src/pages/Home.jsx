2c7e0650557c924b7fbc07eac1c04df57fed4d92
lillianaleecode <lilliana.lee@code.berlin>
2022-07-15 16:15:20 +0200

    integrating Product in Home Page and changing source of the Material UI Icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Categories from '../components/Categories'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import Newsletter from '../components/Newsletter'
+import Products from '../components/Products'
 import Slider from '../components/Slider'
 
 export const Home = () => {
@@ -13,6 +14,7 @@ export const Home = () => {
       <Navbar/>
       <Slider/>
       <Categories/>
+      <Products/>
       <Newsletter/>
       <Footer/>
 
