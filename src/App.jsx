6eff61d9e98b371b7df5b80ff007c6c918f20293
lillianaleecode <lilliana.lee@code.berlin>
2022-07-15 18:07:16 +0200

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 //import { Home } from "./pages/Home";
 
 //testing productList page:
+import Login from "./pages/Login";
 import  ProductList  from "./pages/ProductList";
 import Register from "./pages/Register";
 
 
 const App = () => {
-  return <Register/>;
+  return <Login/>;
 };
 
 export default App;
